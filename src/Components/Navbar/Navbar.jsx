e9fac47a0f404912f4bf805655d11d646b22761e
AryanMadhu <aryansmadhu@gmail.com>
2024-06-30 17:58:56 +0530

    updated navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import './Navbar.css'
-import logo from '../assets/'
+// import logo from '../assets'
 
-export const Navbar = () => {
+const Navbar = () => {
+    
+    const [menu,setmenu] = useState("home");
+
   return (
     <div className="navbar">
         <div className="nav-logo">
-            <img src={logo} alt="" />
+            {/* <img src={logo} alt="" /> */}
             <p>WellnessWave</p>
         </div>
-        <ul className=''></ul>
+        <ul className='nav-menu'>
+            <li onClick={()=>{setmenu("home")}}>Home{menu==="home"?<hr/>:<></>}</li>
+            <li onClick={()=>{setmenu("about")}}>About Us{menu==="about"?<hr/>:<></>}</li>
+            <li onClick={()=>{setmenu("help")}}>Help{menu==="help"?<hr/>:<></>}</li>
+            <button>Login</button>
+            <button>Sign Up</button>
+        </ul>
     </div>
   )
 }
+export default Navbar;
