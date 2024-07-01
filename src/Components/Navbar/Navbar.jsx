e75840bf1a36f63dd8ee7fc08e4b2b4193974025
AryanMadhu <aryansmadhu@gmail.com>
2024-07-01 21:22:18 +0530

    updated navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Navbar.css'
 // import logo from '../assets'
+import {Link} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 
 const Navbar = () => {
     
-    const [menu,setmenu] = useState("home");
+    // const [menu,setmenu] = useState("home");
+    const navigate = useNavigate()
+    const handleOnClick1 = () => {
+        navigate('/login')
+    }
+    const handleOnClick2 = () => {
+        navigate('/signup')
+    }
 
   return (
     <div className="navbar">
@@ -13,11 +22,15 @@ const Navbar = () => {
             <p>WellnessWave</p>
         </div>
         <ul className='nav-menu'>
-            <li onClick={()=>{setmenu("home")}}>Home{menu==="home"?<hr/>:<></>}</li>
+            <Link to="/">Home</Link>
+            <Link to="/about">About Us</Link>
+            <Link to="/help">Help</Link>
+
+            {/* <li onClick={()=>{setmenu("home")}}>Home{menu==="home"?<hr/>:<></>}</li>
             <li onClick={()=>{setmenu("about")}}>About Us{menu==="about"?<hr/>:<></>}</li>
-            <li onClick={()=>{setmenu("help")}}>Help{menu==="help"?<hr/>:<></>}</li>
-            <button>Login</button>
-            <button>Sign Up</button>
+            <li onClick={()=>{setmenu("help")}}>Help{menu==="help"?<hr/>:<></>}</li> */}
+            <button onClick={handleOnClick1}>Login</button>
+            <button onClick={handleOnClick2}>Sign Up</button>
         </ul>
     </div>
   )
