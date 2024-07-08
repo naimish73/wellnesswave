f45494fb4373803882893389564d921491c95e5b
AryanMadhu <aryansmadhu@gmail.com>
2024-07-08 15:57:14 +0530

    corrected various things

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { useAuth } from '../utils/AuthContext'
+import { useAuth } from '../../utils/AuthContext'
 
 
 const Login = () => {
